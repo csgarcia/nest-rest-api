42167162a8f9237cec5b6fbf64fa1c14db834b1f
Carlos García <carlos@hugoapp.com>
2021-12-28 00:50:52 -0600

    - Adding POST product service implementation - Adding product module mongo connection

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { ProductController } from './product.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product, ProductSchema } from './schema/product.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeatureAsync([
+      { name: Product.name, useFactory: () => ProductSchema },
+    ]),
+  ],
   providers: [ProductService],
-  controllers: [ProductController],
+  exports: [ProductService],
 })
 export class ProductModule {}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,13 +1,67 @@
 import { Injectable, HttpStatus } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product, ProductDocument } from './schema/product.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ProductService {
-  async saveProduct() {
-    return {
-      code: HttpStatus.CREATED,
-      data: {
-        message: 'Product created',
-      },
-    };
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+  ) {}
+
+  /**
+   * function to save product
+   * @param {Object} productData
+   * @param {string} productData.sku
+   * @param {string} productData.name
+   * @param {string} productData.description - optional
+   * @param {number} productData.price
+   */
+  async saveProduct(productData) {
+    try {
+      const { sku, name, description = '', price } = productData;
+      // validate params
+      if (!sku || !name || !price) {
+        return {
+          success: false,
+          code: 400,
+          data: {
+            message:
+              'Missing params, please check if sku: string, name: string or price: number params are correct',
+          },
+        };
+      }
+      // create product
+      const productCreated = await this.productModel.create({
+        sku,
+        name,
+        price,
+        description,
+      });
+      if (!productCreated) {
+        return {
+          success: false,
+          code: 500,
+          data: {
+            message: 'Error, product could not be created',
+          },
+        };
+      }
+      return {
+        success: true,
+        code: HttpStatus.CREATED,
+        data: {
+          id: productCreated._id.toString(),
+        },
+      };
+    } catch (e) {
+      return {
+        success: false,
+        code: 500,
+        data: {
+          message: `Error in saveProduct, ${e.message}`,
+        },
+      };
+    }
   }
 }
