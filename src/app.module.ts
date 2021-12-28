efbbb7f226c450bd94649f748e636cac30994b4d
Carlos García <carlos@hugoapp.com>
2021-12-27 22:24:23 -0600

    - Adding mongo connection for data persistence - Adding config module for env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductExtrasModule } from './product-extras/product-extras.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_CONNECTION),
+    ProductExtrasModule,
+  ],
 })
 export class AppModule {}
